ffff2ad8a2c313fc8034de94f1408230fa7010d6
otonye7 <otonyeamie@gmail.com>
2022-02-11 17:45:35 +0100

    master

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,9 @@
 const register = async (req, res) => {
-    res.send('HELLO I AM HERE LEARNING')
+    res.send('HELLO I AM HERE LEARNING');
 }
 
 const login = async (req, res) => {
-    res.send('HELLO I AM HERE LEARNING')
+    res.send('HELLO I AM HERE LEARNING');
 }
 
 module.exports =  {
