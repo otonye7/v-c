e7f3d048d4de07f4a68fb95f29d5682dea4f41dd
otonye7 <otonyeamie@gmail.com>
2022-02-12 21:57:30 +0100

    first

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,5 +3,7 @@ const jwt = require('jsonwebtoken');
 const config = process.env
 
 const verifyToken = (req, res, next) => {
+    let token = req.body.token || req.query.token || req.headers['authorization'];
     
+
 }
