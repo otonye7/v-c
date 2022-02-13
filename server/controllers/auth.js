a488395b415a96a402bbc25bb3a6d33a24853d83
otonye7 <otonyeamie@gmail.com>
2022-02-13 23:18:07 +0100

    master

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 const register = async (req, res) => {
     try {
